cb4e233c044fd5ce4d40bfe4312923b40def927f
Diana Alejandra Radu Giju <dgijrad2710@g.educaand.es>
2025-09-25 14:20:56 +0200

    Script Verificación de elementos y estructura básica completado

--- scripts/script.bash
+++ scripts/script.bash
@@ -1,39 +1,54 @@
 #!/bin/bash
+
+#Variables
 elementos=("git" "jq" "node" "npm" "curl")
-for i in elementos 
-do
-    if [!(i --version)] then;
-        echo "ERROR: El archivo $i no existe"
+arch=(".env.example" ".gitignore" "README.md")
+dir=("src" "scripts")
+USER=$(whoami)
+PROYECTO="semana01"
+
+#Verificar herramientas
+for herramienta in "${elementos[@]}"
+do 
+    if command -v "$herramienta" &> /dev/null; then
+        echo "Herramienta '$herramienta' encontrada"
+    else
+        echo "ERROR: La herramienta '$herramienta' no está instalada"
         exit 1
     fi
 done
 
-USER=$(whoami)
-arch=("env.example" ".gitignore" "README.md")
-dir=("src" "scripts")
-function verificarArchivos(proyecto){
-    cd /c/Users/$USER/Documents/$proyecto
-    for (i in arch)
-    do
-    $existe=false;
-    if (-f arch[i]); then
-        $existe=true;
-    else 
-        $existe=false;
-        exit 1
+#Verificar Archivos y Directorios
+function verificarEstructura(){
+    local tipo="$1"
+    local lista=()
+    local comprobacion=""
+
+    if [ "$tipo" = "archivo" ]; then
+        lista=("${arch[@]}")
+        comprobacion="-f"
+    elif [ "$tipo" = "directorio" ]; then
+        lista=("${dir[@]}")
+        comprobacion="-d"
     fi
+
+    for valor in "${lista[@]}"
+    do
+        if [ "$comprobacion" "$valor" ]; then
+            echo "$valor encontrado."
+        else 
+            echo "ERROR: $tipo requerido '$valor' no encontrado" >&2
+            exit 1
+        fi
     done
 }
-function verificarDirectorios(proyecto){
-    cd /c/Users/$USER/Documents/$proyecto
-    for (i in dir)
-    do
-    $existe=false;
-    if (-f dir[i]); then
-        $existe=true;
-    else 
-        $existe=false;
-        exit 1
-    fi
-    done
-}
+
+    cd "/c/Users/$USER/Documents/$PROYECTO" || {
+    echo "ERROR: No se pudo entrar al directorio del proyecto"
+    exit 1
+    }
+    verificarEstructura "archivo"
+    verificarEstructura "directorio"
+
+    echo "VERIFICACIÓN COMPLETADA CON ÉXITO"
+    exit 0
